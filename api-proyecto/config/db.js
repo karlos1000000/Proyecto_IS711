343f06aa8b80a6f71dc85b8f9545c32bb6fb4a78
ctrejom <ctrejom@unah.hn>
2024-12-04 01:42:25 -0600

    union ramas

--- api-proyecto/config/db.js
+++ api-proyecto/config/db.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2';
 import 'dotenv/config';
 
 const connection = mysql.createConnection({
-    host: process.env.HOST2,
-    port: process.env.PORTDB2, // reemplazar con el puerto de la base de datos
-    user: process.env.USER2, // reemplazar con el usuario de la base de datos
-    password: process.env.PASSWORD2, // contraseña del usuario de la base de datos
-    database: process.env.DATABASE2, // reemplazar con el nombre de la base de datos
+    host: process.env.HOST,
+    port: process.env.PORTDB, // reemplazar con el puerto de la base de datos
+    user: process.env.USER, // reemplazar con el usuario de la base de datos
+    password: process.env.PASSWORD, // contraseña del usuario de la base de datos
+    database: process.env.DATABASE, // reemplazar con el nombre de la base de datos
 })
 
 connection.connect((error) => {
