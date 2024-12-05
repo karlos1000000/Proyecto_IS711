3f4c4d3916a3664706893ebecd475d182f4f0b85
ctrejom <ctrejom@unah.hn>
2024-12-05 14:39:35 -0600

    Añadimos ambos Middlewares para permitir solamente usuarios autenticados y que son administradores

--- api-proyecto/routes/inventory.js
+++ api-proyecto/routes/inventory.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { inventoryController } from '../controllers/inventoryControllers.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
+import isAdmin from '../middlewares/isAdmin.js';
 
 
 const inventoryRouter = Router();
-
+inventoryRouter.use([authMiddleware, isAdmin])
 inventoryRouter.post('/restock', inventoryController.restockInventory);//Restock Inventario
 inventoryRouter.get('/:id', inventoryController.getInventoryById);//Consultar Inventario por ID
 
--- api-proyecto/routes/reports.js
+++ api-proyecto/routes/reports.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { reportsController } from '../controllers/reportsControllers.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
+import isAdmin from '../middlewares/isAdmin.js';
 
 
 const reportRouter = Router();
-
-reportRouter.get('/inventory', reportsController.reportInventory)
-reportRouter.get('/sales/:start_date/:end_date', reportsController.reportSales)
+reportRouter.use([authMiddleware, isAdmin])
+reportRouter.get('/inventory',reportsController.reportInventory)
+reportRouter.get('/sales/:start_date/:end_date' , reportsController.reportSales)
 
 export default reportRouter;
