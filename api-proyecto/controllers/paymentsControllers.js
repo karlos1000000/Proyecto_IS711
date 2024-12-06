905ffdb5ebd55ee5c50389e35a35e38ba61c8f30
Gerson <gersonmurillo2711@gmail.com>
2024-12-06 01:42:10 -0600

    Creacion del codigo para el payment.js y el paymentscontroller.js

--- api-proyecto/controllers/paymentsControllers.js
+++ api-proyecto/controllers/paymentsControllers.js
@@ -1,9 +1,138 @@
-
+import db from '../config/db.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
 
 export class paymentController{
 
 
     static postPayment = (req, res) => {
 
+    }
+
+    static checkOut = (req, res) => {
+        const consultaPago = `INSERT INTO Pago (User_id, total, status) VALUES ( ?, ?, ?)`;
+        const ObtenerIdPago = `SELECT id FROM Pago WHERE User_id = ? AND total = ? AND status = ?`;
+        const consultaPago_Detalle = `INSERT INTO Pago_Detalle (payment_id, product_id, cantidad, price) VALUES (?, ?, ?, ?)`; 
+                        
+        const data = req.body;
+
+        try {
+            db.query(consultaPago, [data.User_id, data.total, data.status], (err, results) => {
+                
+                if (err) {
+                    return res.status(400)
+                            .json({
+                                message: "Error al realizar el pago (error en el query)" + err,
+                                error: true
+                    });
+                } 
+
+                if (results.affectedRows === 0) {
+                    return res.status(400)
+                            .json({
+                                message: "No se realizó el pago",
+                                error: true
+                            });
+                }
+
+                db.query(ObtenerIdPago, [data.User_id, data.total, data.status], (err, results) => {
+                    if (err) {
+                        return res.status(400)
+                                .json({
+                                    message: "Error al obtener el id del pago (error en el query)" + err,
+                                    error: true
+                        });
+                    } 
+
+                    if (results.length === 0) {
+                        return res.status(400)
+                                .json({
+                                    message: "No se encontró el id del pago",
+                                    error: true
+                                });
+                    }
+
+                    const payment_id = results[0].id;
+
+                    data.products.forEach(product => {
+                        db.query(consultaPago_Detalle, [payment_id, product.id, product.cantidad, product.price], (err, results) => {
+                            if (err) {
+                                return res.status(400)
+                                        .json({
+                                            message: "Error al realizar el pago (error en el query)" + err,
+                                            error: true
+                                });
+                            } 
+
+                            if (results.affectedRows === 0) {
+                                return res.status(400)
+                                        .json({
+                                            message: "No se realizó el pago",
+                                            error: true
+                                        });
+                            }
+                        });
+                    });
+
+                    return res.header("Content-Type", "application/json")
+                            .status(200)
+                            .json({
+                                message: "Pago realizado exitosamente",
+                                paymentId: payment_id
+                            });
+                });
+            });
+
+        } catch (error) {
+            
+            return res.status(400)
+                    .json({
+                        message: "Error al realizar el pago (error en el catch)",
+                        error: true
+            });
+        }
+    }
+
+    static getPaymentHistory = (req, res) => {
+        const { id } = req.params;
+        const consulta = "SELECT id, total, userId, created_date FROM Pago WHERE userId = ?";
+
+        try {
+           
+            db.query(consulta, [id], (err, results) => {
+                
+                if (err) {
+                    return res.status(400)
+                            .json({
+                                message: "Error al obtener el historial de pagos (error en el query)" + err,
+                                error: true
+                    });
+                }
+
+                if (results.length === 0) {
+                    return res.status(400)
+                            .json({
+                                message: "No se encontró el historial de pagos",
+                                error: true
+                            });
+                }
+
+                return res.header("Content-Type", "application/json")
+                        .status(200)
+                        .json({
+                            message: "Historial de pagos",
+                            payments: results
+                        });
+            });
+        } catch (error) {
+            
+            return res.status(400)
+                    .json({
+                        message: "Error al obtener el historial de pagos (error en el catch)",
+                        error: true
+            });
+    
+        }
     }
 }
--- api-proyecto/routes/payments.js
+++ api-proyecto/routes/payments.js
@@ -1,7 +1,13 @@
 import Transaction from '@pixelpay/sdk-core'
+import { Router } from 'express'
+import { paymentController } from '../controllers/paymentController.js';
+import isClient from '../middlewares/isClient.js';
 
 const paymentRouter = Router();
 
+inventoryRouter.use([authMiddleware, isClient])
 paymentRouter.post('https://pixel-pay.com/api/v2/transaction/sale', paymentController.postPayment);
+paymentRouter.post('/checkout', authController.registerUser);//Procesar pago
+paymentRouter.get('/history/:id', authController.loginUser);// Obtener historial de pagos
 
 export default paymentRouter;
